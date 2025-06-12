d8714470f840547f5cef839d7d513b8697bc25f7
Apratim-Haldar <apratim.haldar2022@iem.edu.in>
2025-06-12 12:56:56 +0530

    some models and case types created during testing

--- core/case-type/information-request.js
+++ core/case-type/information-request.js
@@ -53,7 +53,7 @@ class InformationRequestCase extends BlazeCase {
         "type": "manual"
       }
     ]
-  },
+  }
 ]
     );
   }
---
+++ core/data-model/testing-model.js
@@ -0,0 +1,8 @@
+module.exports = {
+  id: 'testing-model',
+  label: 'Testing Model',
+  description: 'This is a test model to run analysis during development.',
+  extends: 'blaze-entity',
+  isEditable: true,
+  attributes: []
+};
